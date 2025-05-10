b89ff95d88040aeb4cf11306c927401ea71e52f7
Denkut <107022817+Denkut@users.noreply.github.com>
2025-05-10 13:08:20 +0300

    add likes player and add projects gif

---
+++ src/constants/previews.ts
@@ -0,0 +1,17 @@
+export const previews = [
+  {
+    id: "upfood",
+    name: "UpFood",
+    preview: "/previews/upfood.gif",
+  },
+  {
+    id: "news",
+    name: "News Reactify",
+    preview: "/previews/news.gif",
+  },
+  {
+    id: "kutepova",
+    name: "Kutepova",
+    preview: "/previews/kutepova.gif",
+  },
+];
